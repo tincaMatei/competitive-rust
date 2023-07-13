11e99e7068b5e27253edbde7a1992d0adf5c47b8
Matei Tinca <14098249+tincaMatei@users.noreply.github.com>
2023-07-13 15:30:53 +0200

    Rewrote graph utilities

--- src/graph.rs
+++ src/graph.rs
@@ -1,207 +1,412 @@
-//! A collection of graph utilities and algorithms.
-//!
-//! Usually, you would need to create a list that uses adjacency lists. 
-//! You can either to it immutably, by passing the edges used
-//! or mutably, by pushing the edges yourself.
-//!
-//! ```
-//! # use competitive_rust::graph::{AdjListGraph, Graph};
-//! #
-//! let edges = vec![(0, 1), (0, 2), (1, 2)];
-//! let graph = AdjListGraph::from_undirected_edges(3, &edges);
-//!
-//! for node in graph.neighbours(0) {
-//!     println!("Edge: {} -> {}", 0, node);
-//! }
-//!
-//! assert_eq!(graph.neighbours(1).map(|e| *e).collect::<Vec<usize>>(), vec![0, 2]);
-//! ```
+//! Graph utilities.
 
-use std::slice::Iter;
+/// Trait for implementing an edge.
+pub trait Edge {
+    /// Returns the node that this edge points to.
+    ///
+    /// `from` is required for bidirectional edges, otherwise the user wouldn't know in which
+    /// direction to walk the edge.
+    fn to(&self, from: usize) -> usize;
+}
 
-/// A trait for representing graphs, regardless of whether they're directed or undirected.
-pub trait Graph<'a, T>
-where T: 'a + Copy {
-    /// Returns the number of nodes in the graph.
-    fn v(&self) -> usize;
+/// Trait for implementing bidirectional edges.
+pub trait BidirectionalEdge: Edge {
+    /// Returns the pair of nodes that it connects.
+    fn as_pair(&self) -> (usize, usize);
+}
 
-    /// Returns the number of edges in the graph.
-    fn e(&self) -> usize;
+/// Data structure to represent graphs.
+#[derive(Debug)]
+pub struct Graph<V, E>
+where V: Default,
+      E: Edge {
+    /// Data to be stored inside every node. Usually it's going to be [()]
+    pub nodes: Vec<V>,
+
+    /// Every edge of the graph.
+    pub edges: Vec<E>,
+
+    /// Adjacency list. Every entry in this vector refers to the index in the [edges] vector.
+    pub adj_list: Vec<Vec<usize>>,
+
+    /// `true` if the graph is undirected, `false` otherwise.
+    pub undirected: bool,
+}
+
+impl<V, E> Graph<V, E> 
+where V: Default,
+      E: Edge {
     
-    /// Returns the neighbours of a node as an iterator.
-    fn neighbours(&'a self, node: T) -> Iter<'a, T>;
+    /// Create a graph from the edge vector.
+    ///
+    /// `v` is the number of nodes in the graph.
+    ///
+    /// `edges` is the vector of edges to create the graph from. This should be a
+    /// [BidirectionalEdge] so that the function know to whom to add the edge.
+    ///
+    /// `transf` is a closure that transforms each bidirectional edge to the current type of edge.
+    /// This is used mostly to reduce the memory used. For instance, instead of storing a pair for
+    /// each of the edges, you can store just the destination of the edge, because the other one is
+    /// implicit.
+    ///
+    /// `undirected` should be `true` if the graph is undirected.
+    pub fn from_edges<T, F>(
+        v: usize, 
+        edges: Vec<T>, 
+        transf: F,
+        undirected: bool
+    ) -> Graph<V, E>
+    where T: BidirectionalEdge,
+          F: Fn(T) -> E {
+        let mut nodes = Vec::with_capacity(v);
+        for _ in 0..v {
+            nodes.push(V::default());
+        }
 
-    /// Returns the set of nodes.
-    fn nodes(&self) -> Vec<T>;
-
-    /// Returns the set of edges;
-    fn edges(&'a self) -> Vec<(T, T)> {
-        let mut edges = Vec::<(T, T)>::new();
-
-        for node in self.nodes() {
-            for neigh in self.neighbours(node) {
-                edges.push((node, *neigh));
+        let mut capacity = vec![0; v];
+        for e in &edges {
+            let (a, b) = e.as_pair();
+            capacity[a] += 1;
+            if undirected {
+                capacity[b] += 1;
             }
         }
 
-        edges
-    }
-}
-
-/// Generic implementation for graphs.
-///
-/// By default, this is a directed graph. To make it an undirected graph,
-/// consider the graph as a directed one.
-///
-/// Any function that uses this graph as an undirected graph will panic if this
-/// is not actually an undirected graph.
-///
-/// The nodes are numbered from 0 to V - 1.
-pub struct AdjListGraph {
-    pub v: usize,
-    pub adj: Vec<Vec<usize>>,
-}
-
-impl<'a> Graph<'a, usize> for AdjListGraph {
-    fn v(&self) -> usize { self.adj.len() }
-    fn e(&self) -> usize { self.adj.iter().fold(0, |ac, e| { ac + e.len() } ) }
-    
-    fn nodes(&self) -> Vec<usize> {
-        (0..self.v()).collect()
-    }
-
-    fn neighbours(&'a self, node: usize) -> Iter<'a, usize> {
-        self.adj[node].iter()
-    }
-}
-
-impl AdjListGraph {
-    /// Construct a graph with `v` nodes and no edges.
-    pub fn empty(v: usize) -> AdjListGraph {
-        AdjListGraph {
-            v,
-            adj: vec![vec![]; v],
-        }
-    } 
-
-    /// Construct a graph with `v` nodes and the given edges.
-    pub fn from_edges(v: usize, edges: &[(usize, usize)]) -> AdjListGraph {
-        let mut g = AdjListGraph::empty(v);
-
-        for edge in edges {
-            g.push_edge(edge.0, edge.1);
+        let mut adj_list: Vec<Vec<usize>> = Vec::with_capacity(v);
+        for i in 0..v {
+            adj_list.push(Vec::with_capacity(capacity[i]));
         }
 
-        g
-    } 
+        for id in 0..edges.len() {
+            let edge = &edges[id];
+            let (a, b) = edge.as_pair();
+            adj_list[a].push(id);
 
-    /// Construct a graph with `v` nodes and the given undirected edges.
-    pub fn from_undirected_edges(v: usize, edges: &[(usize, usize)]) -> AdjListGraph {
-        let mut g = AdjListGraph::empty(v);
-
-        for edge in edges {
-            g.push_undirected_edge(edge.0, edge.1);
+            if undirected {
+                adj_list[b].push(id);
+            }
         }
+        
+        let edges: Vec<E> = edges.into_iter().map(transf).collect();
 
-        g
-    } 
+        Graph::<V, E> {
+            nodes,
+            edges,
+            adj_list,
+            undirected
+        }
+    }
+
+    /// Create an empty graph with capacities for the nodes and edges.
+    pub fn with_capacity(v: usize, e: usize, undirected: bool) -> Graph<V, E> {
+        Graph::<V, E> {
+            nodes: (0..v).map(|_| V::default()).collect(),
+            edges: Vec::with_capacity(e),
+            adj_list: vec![Vec::new(); v],
+            undirected
+        }
+    }
 
     /// Push a directed edge in the graph.
-    pub fn push_edge(&mut self, a: usize, b: usize) {
-        self.adj[a].push(b);
+    pub fn push_directed_edge(&mut self, from: usize, edge: E) {
+        let id = self.edges.len();
+        self.edges.push(edge);
+        self.adj_list[from].push(id);
     }
 
     /// Push an undirected edge in the graph.
-    pub fn push_undirected_edge(&mut self, a: usize, b: usize) {
-        self.push_edge(a, b);
-        self.push_edge(b, a);
+    pub fn push_undirected_edge(&mut self, edge: E)
+    where E: BidirectionalEdge {
+        let id = self.edges.len();
+        let (a, b) = edge.as_pair();
+
+        self.edges.push(edge);
+        self.adj_list[a].push(id);
+        self.adj_list[b].push(id);
     }
 
-    /// Check if the graph is undirected.
-    ///
-    /// If you need to assert that a given graph is undirected, don't use
-    /// [assert], use [debug_assert] instead.
-    pub fn is_undirected(&self) -> bool {
-        use std::collections::HashMap;
+    /// Return the number of nodes.
+    pub fn v(&self) -> usize { self.nodes.len() }
+    
+    /// Return the number of edges.
+    pub fn e(&self) -> usize { self.edges.len() }
+}
 
-        let mut hashmap = HashMap::<(usize, usize), usize>::new();
-        
-        for edge in self.edges() {
-            let entry = *hashmap.entry(edge)
-                .or_insert(0);
-            hashmap.insert(edge, entry + 1);
-        }
+impl Edge for usize {
+    fn to(&self, _: usize) -> usize { *self }
+}
 
-        for edge in self.edges() {
-            if hashmap.get(&edge) != hashmap.get(&(edge.1, edge.0)) {
-                return false;
-            }
-        }
-
-        return true;
+impl Edge for (usize, usize) {
+    fn to(&self, from: usize) -> usize {
+        return self.0 ^ self.1 ^ from;
     }
 }
+
+impl BidirectionalEdge for (usize, usize) {
+    fn as_pair(&self) -> (usize, usize) {
+        return *self;
+    }
+}
+
+struct EulerianCycleSolver<'a, V, E>
+where V: Default,
+      E: Edge {
+    
+    graph: &'a Graph<V, E>,
+    used_edge: Vec<bool>,
+    last_edge: Vec<usize>,
+    result: Vec<usize>,
+}
+
+impl<'a, V, E> EulerianCycleSolver<'a, V, E>
+where V: Default,
+      E: Edge {
+    
+    fn eulerian_dfs(&mut self, node: usize) {
+        while self.last_edge[node] < self.graph.adj_list[node].len() {
+            let cnt_edge = self.last_edge[node];
+            let id = self.graph.adj_list[node][cnt_edge];
+            
+            if !self.used_edge[id] {
+                let other = self.graph.edges[id].to(node);
+                self.used_edge[id] = true;
+
+                self.eulerian_dfs(other);
+            }
+
+            self.last_edge[node] += 1; 
+        }
+
+        self.result.push(node);
+    }
+}
+
+/// Find an eulerian path in the given graph. `cycle` should be true if you want to find a cycle,
+/// otherwise it will find just a path. Returns [None] if there is no eulerian cycle/path. You can
+/// also override whether the graph is directed or not. Use this function if you want to override
+/// the `undirected` field.
+pub fn solve_eulerian<V, E>(
+    graph: &Graph<V, E>,
+    undirected: bool,
+    cycle: bool
+) -> Option<Vec<usize>>
+where V: Default,
+      E: Edge{
+    
+    let mut degree = vec![0; graph.v()];
+
+    for node in 0..graph.v() {
+        for it in &graph.adj_list[node] {
+            let edge = &graph.edges[*it];
+            degree[edge.to(node)] += 1;
+        }
+    }
+
+    let mut start_node = 0;
+    let mut cnt_even = 0;
+    let mut cnt_pos = 0;
+    let mut cnt_neg = 0;
+
+    for node in 0..graph.v() {
+        if undirected && degree[node] % 2 == 1 {
+            start_node = node;
+            cnt_even += 1;
+        } else if !undirected && degree[node] < graph.adj_list[node].len() {
+            start_node = node;
+            cnt_pos += 1;
+        } else if !undirected && degree[node] > graph.adj_list[node].len() {
+            cnt_neg += 1;
+        }
+    }
+
+    if undirected {
+        if cnt_even > 2 {
+            return None;
+        }
+        if cycle && cnt_even > 0 {
+            return None;
+        }
+    } else {
+        if cnt_pos > 1 || cnt_neg > 1 {
+            return None;
+        }
+        if cycle && (cnt_pos > 0 || cnt_neg > 0) {
+            return None;
+        }
+    }
+    
+    let mut solver = EulerianCycleSolver {
+        graph,
+        used_edge: vec![false; graph.e()],
+        last_edge: vec![0; graph.v()],
+        result: Vec::with_capacity(graph.e() + 1),
+    };
+
+    solver.eulerian_dfs(start_node);
+
+    if solver.result.len() != graph.e() + 1 {
+        None
+    } else {
+        if !undirected {
+            solver.result.reverse();
+        }
+        Some(solver.result)
+    }
+}
+
+/// Find an eulerian cycle in the graph.
+pub fn find_eulerian_cycle<V, E>(graph: &Graph<V, E>) -> Option<Vec<usize>>
+where V: Default,
+      E: Edge {
+    solve_eulerian(graph, graph.undirected, true)
+}
+
+/// Find an eulerian path in the graph.
+pub fn find_eulerian_path<V, E>(graph: &Graph<V, E>) -> Option<Vec<usize>>
+where V: Default,
+      E: Edge {
+    solve_eulerian(graph, graph.undirected, false)
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn graph_v_empty() {
-        let g = AdjListGraph::empty(10);
-
-        assert_eq!(g.v(), 10);
+    fn unordered_edge(a: usize, b: usize) -> (usize, usize) {
+        if a < b {
+            (a, b)
+        } else {
+            (b, a)
+        }
     }
 
     #[test]
-    fn graph_e_empty() {
-        let g = AdjListGraph::empty(10);
+    fn test_eulerian_undirected_cycle() {
+        let mut edges: Vec<(usize, usize)> = vec![
+            (0, 1),
+            (0, 2),
+            (1, 1),
+            (1, 2),
+            (2, 3),
+            (2, 3)
+        ];
 
-        assert_eq!(g.e(), 0);
+        let graph = Graph::<(), (usize, usize)>::from_edges(
+            4,
+            edges.clone(),
+            |x| { x },
+            true
+        );
+
+        let cycle = find_eulerian_cycle(&graph).unwrap();
+
+        let mut cycle_edges = Vec::new();
+        for i in 0..cycle.len() - 1 {
+            cycle_edges.push(unordered_edge(cycle[i], cycle[i + 1]));
+        }
+
+        cycle_edges.sort();
+        edges.sort();
+    
+        assert_eq!(edges, cycle_edges);
     }
-
+    
     #[test]
-    fn directed_graph_count() {
-        let edges = vec![(0, 1), (1, 2)];
-        let g = AdjListGraph::from_edges(3, &edges);
+    fn test_eulerian_undirected_path() {
+        let mut edges: Vec<(usize, usize)> = vec![
+            (0, 2),
+            (1, 1),
+            (1, 2),
+            (2, 3),
+            (2, 3),
+        ];
 
-        assert_eq!(g.e(), edges.len());
+        let graph = Graph::<(), (usize, usize)>::from_edges(
+            4,
+            edges.clone(),
+            |x| { x },
+            true
+        );
+
+        let cycle = find_eulerian_path(&graph).unwrap();
+
+        let mut cycle_edges = Vec::new();
+        for i in 0..cycle.len() - 1 {
+            cycle_edges.push(unordered_edge(cycle[i], cycle[i + 1]));
+        }
+
+        cycle_edges.sort();
+        edges.sort();
+    
+        assert_eq!(edges, cycle_edges);
     }
-
+    
     #[test]
-    fn undirected_edge_count() {
-        let edges = vec![(0, 1), (1, 2)];
-        let g = AdjListGraph::from_undirected_edges(3, &edges);
+    fn test_eulerian_directed_cycle() {
+        let mut edges: Vec<(usize, usize)> = vec![
+            (0, 1),
+            (1, 2),
+            (2, 0),
+            (0, 3),
+            (3, 4),
+            (4, 0),
+            (0, 5),
+            (5, 6),
+            (6, 0)
+        ];
 
-        assert_eq!(g.e(), edges.len() * 2);
+        let graph = Graph::<(), usize>::from_edges(
+            7,
+            edges.clone(),
+            |x| { x.1 },
+            false
+        );
+
+        let cycle = find_eulerian_cycle(&graph).unwrap();
+
+        let mut cycle_edges = Vec::new();
+        for i in 0..cycle.len() - 1 {
+            cycle_edges.push((cycle[i], cycle[i + 1]));
+        }
+
+        cycle_edges.sort();
+        edges.sort();
+
+        assert_eq!(edges, cycle_edges);
     }
-
+    
     #[test]
-    fn test_undirected_adjacency() {
-        let edges = vec![(0, 1), (2, 0)];
-        let g = AdjListGraph::from_undirected_edges(3, &edges);
+    fn test_eulerian_directed_path() {
+        let mut edges: Vec<(usize, usize)> = vec![
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 2),
+            (2, 4),
+            (4, 3),
+            (3, 2),
+            (2, 4)
+        ];
 
-        assert_eq!(g.neighbours(0).map(|e| *e).collect::<Vec<usize>>(), vec![1, 2]);
-        assert_eq!(g.neighbours(1).map(|e| *e).collect::<Vec<usize>>(), vec![0]);
-        assert_eq!(g.neighbours(2).map(|e| *e).collect::<Vec<usize>>(), vec![0]);
-    }
+        let graph = Graph::<(), usize>::from_edges(
+            5,
+            edges.clone(),
+            |x| { x.1 },
+            false
+        );
 
-    #[test]
-    fn test_directed_adjacency() {
-        let edges = vec![(0, 1), (2, 0)];
-        let g = AdjListGraph::from_edges(3, &edges);
+        let cycle = find_eulerian_path(&graph).unwrap();
 
-        assert_eq!(g.neighbours(0).map(|e| *e).collect::<Vec<usize>>(), vec![1]);
-        assert_eq!(g.neighbours(1).map(|e| *e).collect::<Vec<usize>>(), vec![]);
-        assert_eq!(g.neighbours(2).map(|e| *e).collect::<Vec<usize>>(), vec![0]);
-    }
+        let mut cycle_edges = Vec::new();
+        for i in 0..cycle.len() - 1 {
+            cycle_edges.push((cycle[i], cycle[i + 1]));
+        }
 
-    #[test]
-    fn test_nodes() {
-        let g = AdjListGraph::empty(10);
+        cycle_edges.sort();
+        edges.sort();
 
-        assert_eq!(g.nodes(), vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(edges, cycle_edges);
     }
 }
-
 
